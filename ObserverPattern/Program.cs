fcf7cc8fcb858d3cc860b530282f50bb077ebc72
Eduardo  Velez <Eduardo@Eduardos-MacBook-Pro-2.local>
2017-06-05 10:07:36 -0400

    Obseerver Complete

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -6,7 +6,10 @@ namespace ObserverPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WeatherData weatherData = new WeatherData();
+            CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
+
+            weatherData.setMeasurements(80, 65, 30.4f);
         }
     }
 }
--- ObserverPattern/WeatherData.cs
+++ ObserverPattern/WeatherData.cs
@@ -1,17 +1,54 @@
 ﻿using System;
-
-using System.Collections;
+using System.Collections.Generic;
 namespace ObserverPattern
 {
     public class WeatherData : Subject
     {
-        private IList observers;
+        private List<Observer> observers;
         private float temperature;
         private float humidity;
         private float pressure;
+
         public WeatherData()
         {
-            
+            observers = new List<Observer>();
         }
+
+        public void RegisterObserver(Observer o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(Observer o)
+        {
+           //int i = observers.IndexOf(o);
+           //if (i >= 0)
+           // { 
+                observers.Remove(o);    
+            //}
+        }
+
+        public void NotifyObserver()
+        {
+            //int i = 0;
+            //if(i < observers.Count)
+            //{
+                foreach( Observer o in observers)
+                {
+                    o.Update(temperature,humidity,pressure);
+                }
+            //}
+        }
+        public void measurementsChanged()
+        {
+            NotifyObserver();
+        }
+		public void setMeasurements(float temperature, float humidity, float pressure)
+		{
+			this.temperature = temperature;
+			this.humidity = humidity;
+			this.pressure = pressure;
+			measurementsChanged();
+		}
     }
 }
